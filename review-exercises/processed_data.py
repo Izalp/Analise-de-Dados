56448d6c39b3fa11877b8d709e8b5b7fd504fcf5
Iza Lopes <102091381+Izalp@users.noreply.github.com>
2024-10-14 08:15:37 -0300

    :sparkles: feat: Adicionar exercícios de revisão do Numpy

--- review-exercises/processed_data.py
+++ review-exercises/processed_data.py
@@ -32,13 +32,56 @@ def media_literacy(dataset):
     return avg_literacy
 
 
+def cont_northern_america(dataset):
+    data = np.loadtxt(dataset, delimiter=';', dtype=str, skiprows=1, encoding='utf-8')
+    regions = data[:, 1] 
+    regions = np.char.strip(regions) # removendo espaços extras 
+    northern_america_count = np.sum(regions == 'NORTHERN AMERICA')
+
+    print(f"\nNúmero de países na América do Norte (NORTHERN AMERICA): {northern_america_count}")
+
+
+def max_gdp_latin_america(dataset):
+    # Carregar os dados
+    data = np.loadtxt(dataset, delimiter=';', dtype=str, skiprows=1, encoding='utf-8')
+    
+    data[:, 1] = np.char.strip(data[:, 1])  # removendo espaços extras da coluna de regiões
+    
+    latin_america_data = data[data[:, 1] == 'LATIN AMER. & CARIB']
+
+    latin_america_data[:, 8] = np.char.strip(latin_america_data[:, 8]) # removendo espaços extras da coluna GDP 
+    gdp_per_capita = latin_america_data[:, 8] 
+    
+    valid_gdp_mask = gdp_per_capita != '' # Filtra valores não vazios
+    valid_gdp = gdp_per_capita[valid_gdp_mask]  
+    
+    print("\nPaíses encontrados na região LATIN AMER. & CARIB e seu respectivo GDP:")
+    for i, country in enumerate(latin_america_data[valid_gdp_mask, 0]):
+        print(f"{country}: {valid_gdp[i]}")
+
+    valid_gdp = valid_gdp.astype(float)
+
+    max_gdp_index = np.argmax(valid_gdp)
+        
+    # País correspondente ao maior GDP per capita
+    max_gdp_country = latin_america_data[max_gdp_index, 0]
+    print(f"\nPaís da América Latina e Caribe com maior renda per capita (GDP): {max_gdp_country}\n")
+
+
 dataset = 'data/paises.csv'
 
 # 1) Selecionando parte do dataset para mostrar "Country", "Region", "Population", "Area (sq. mi.)":
 paises_output(dataset)
 
 # 2) Contando as regiões no dataset:
+
 cont_regioes(dataset)
 
 # 3) Calculando a taxa média de alfabetização:
 media_literacy(dataset)
+
+# 4) Contando quantos países são da América do Norte (NORTHERN AMERICA):
+cont_northern_america(dataset)
+
+# 5) Encontrando o país com maior renda per capita na América Latina e Caribe:
+max_gdp_latin_america(dataset)
